44e05bd14505819e599cd3dad1dff43b0ef7dc00
ambarcedeno2 <ambarcedeno467@gmailcom>
2023-10-20 22:50:54 -0500

    django portafolio1

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@
 set -o errexit
 
 pip install -r requirements.txt
-DJANGO_SUPERUSER_PASSWORD=1234 python manage.py createsuperuser --no-input --username=ambar1  --email=ambar@example.com
 
 python manage.py collectstatic --no-input
 python manage.py migrate 
